48122f29b8547d5c39751ab6aec9d04e4440d5fd
Hong-Hue Tran <hong.hue.tran@outlook.com>
2023-02-28 18:52:10 -0500

    fixing scalability

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+function navBar() {
+    var x = document.getElementById("navBar");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
